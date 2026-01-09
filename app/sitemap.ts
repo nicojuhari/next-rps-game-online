23fe26a74fdcbf29bdbafa4367153491a339d6f9
Nicolae Cojuhari <nicojuhari@gmail.com>
2026-01-09 19:19:09 +0100

    fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,30 +1,24 @@
-import { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from "next";
+
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: 'https://rps-game.online',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 1,
-    },
-    {
-      url: 'https://rps-game.online/one-player',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://rps-game.online/two-players',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://rps-game.online/privacy',
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 0.3,
-    },
-  ]
+    return [
+        {
+            url: "https://rps-game.online",
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 1,
+        },
+        {
+            url: "https://rps-game.online/two-players",
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 0.8,
+        },
+        {
+            url: "https://rps-game.online/privacy",
+            lastModified: new Date(),
+            changeFrequency: "yearly",
+            priority: 0.3,
+        },
+    ];
 }
--- next.config.ts
+++ next.config.ts
@@ -1,33 +1,33 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.rps-game.online',
-          },
-        ],
-        destination: 'https://rps-game.online/:path*',
-        permanent: true,
-      },
-      {
-        source: '/1-player',
-        destination: '/one-player',
-        permanent: true,
-      },
-      {
-        source: '/2-player',
-        destination: '/two-players',
-        permanent: true,
-      },
-    ];
-  },
-  /* config options here */
+    reactCompiler: true,
+    async redirects() {
+        return [
+            {
+                source: "/:path*",
+                has: [
+                    {
+                        type: "host",
+                        value: "www.rps-game.online",
+                    },
+                ],
+                destination: "https://rps-game.online/:path*",
+                permanent: true,
+            },
+            {
+                source: "/1-player",
+                destination: "/",
+                permanent: true,
+            },
+            {
+                source: "/2-player",
+                destination: "/two-players",
+                permanent: true,
+            },
+        ];
+    },
+    /* config options here */
 };
 
 export default nextConfig;
